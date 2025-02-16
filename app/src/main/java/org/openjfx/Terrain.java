f33b6951edea611563d386f3da77205b040097c6
Harish <hrsh9486@gmail.com>
2025-02-16 15:05:09 +0000

    Cleaned up Terrain abstract class

--- app/src/main/java/org/openjfx/Terrain.java
+++ app/src/main/java/org/openjfx/Terrain.java
@@ -6,19 +6,9 @@ abstract class Terrain {
     private int xcord;
     private int ycord;
     private List<Integer> rgb;
-    private List<Animal> eaters;
     private boolean occupied;
     private Animal occupiedBy;
     private List<Animal> validOccupiers = new ArrayList<Animal>();
-
-    Terrain(int xcord, int ycord, List<Animal> eaters, boolean occupied, List<Integer> rgb, Animal occupiedBy){
-        this.xcord = xcord;
-        this.ycord = ycord;
-        this.eaters = eaters;
-        this.occupied = occupied;
-        this.rgb = rgb;
-        this.occupiedBy = null;
-    }
 
     public void addOccupier(boolean setOccupied, Animal Occupier){
         if ((this.isOccupied() == false) || (this.validOccupiers.contains(Occupier))){
